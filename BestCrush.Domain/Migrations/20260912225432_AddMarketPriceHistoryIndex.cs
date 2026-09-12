e851003b45d16b41ad21229bf0c2c59ed6869720
Pagalom <polaert.cesar@gmail.com>
2026-09-13 00:57:20 +0200

    feat: add market price history foundation

---
+++ BestCrush.Domain/Migrations/20260912225432_AddMarketPriceHistoryIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BestCrush.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMarketPriceHistoryIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_MarketPriceObservations_ObjectType_DofusDbId_ServerName_Quantity_Source_ObservedAtUtc",
+                table: "MarketPriceObservations",
+                columns: new[] { "ObjectType", "DofusDbId", "ServerName", "Quantity", "Source", "ObservedAtUtc" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_MarketPriceObservations_ObjectType_DofusDbId_ServerName_Quantity_Source_ObservedAtUtc",
+                table: "MarketPriceObservations");
+        }
+    }
+}
